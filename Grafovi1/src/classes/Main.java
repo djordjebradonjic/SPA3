d7822d11f4515b6526552046bfb3e1b74ead1b59
djordjebradonjic <djordjebradonjic99@gmail.com>
2026-01-13 01:19:12 +0100

    Algs4 library  added to project and configuration set up

---
+++ Grafovi1/src/classes/Main.java
@@ -0,0 +1,12 @@
+package classes;
+
+import edu.princeton.cs.algs4.Graph;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Graph g= new Graph(6);
+		
+	}
+
+}
--- Grafovi1/src/module-info.java
+++ Grafovi1/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module Grafovi1 {
+class Grafovi1 {
 }
